3437ae5be14b74fb6e8596514d43999d2d93c70a
germanlozickyj <germanlozickyj2003@gmail.com>
2024-08-07 19:55:02 -0300

    upload file in rust

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
 #[macro_use] extern crate rocket;
 
 use rocket::form::Form;
-use rocket::request::FromFormValue;
+use rocket::fs::TempFile;
+use tokio::fs;
+use std::os::unix::fs::PermissionsExt; // Para Unix-specific permissions
 
 #[derive(FromForm)]
-struct MyForm {
-    field: String, 
+struct Upload<'r> {
+    file: TempFile<'r>,
 }
 
-#[post("/format", data = "<form>")]
-fn submit(form: Form<MyForm>) -> String {
-    let field = form.field.clone(); 
-    form.into_inner();
+#[post("/format", data = "<upload>")]
+async fn submit(upload: Form<Upload<'_>>) -> std::io::Result<()> {
+    let temp_path = upload.file.path().expect("No temp path");
 
-    return field;
+    let target_path = "storage/file.png";
+
+    fs::copy(temp_path, target_path).await?;
+
+    let metadata = std::fs::metadata(target_path)?;
+    let mut permissions = metadata.permissions();
+    
+    permissions.set_mode(0o444);
+
+    std::fs::set_permissions(target_path, permissions)?;
+    
+    Ok(())
 }
 
-#[launch]
+#[launch] 
 fn rocket() -> _ {
     rocket::build()
         .mount("/api", routes![submit])
